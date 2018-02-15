be4923bd07fe78ffb74ac028bbf8fa96b1e2644b
Sergey <grafe2210@localhost.localdomain>
2018-02-15 09:36:15 +0700

    complete commit doska.c

--- doska.c
+++ doska.c
@@ -2,5 +2,13 @@
 
 void main()
 {
-    
+    printf("8|L|H|C|F|K|C|H|L|\n");
+    printf("7|P|P|P|P|P|P|P|P|\n");
+    printf("6| |#| |#| |#| |#|\n");
+    printf("5|#| |#| |#| |#| |\n");
+    printf("4| |#| |#| |#| |#|\n");
+    printf("3|#| |#| |#| |#| |\n");
+    printf("2|P|P|P|P|P|P|P|P|\n");
+    printf("1|L|H|C|F|K|C|H|L|\n");
+    printf("  a b c d e f g h\n");
 }
