a905f32290c9c85fbc93dac805b1a793d1e7065d
Sergey <grafe2210@localhost.localdomain>
2018-02-22 08:14:25 +0700

    complete edition doska(putch)

--- doska.c
+++ doska.c
@@ -2,13 +2,13 @@
 
 void main()
 {
-    printf("8|L|H|C|F|K|C|H|L|\n");
-    printf("7|P|P|P|P|P|P|P|P|\n");
+    printf("8|r|n|b|q|k|b|n|r|\n");
+    printf("7|p|p|p|p|p|p|p|p|\n");
     printf("6| |#| |#| |#| |#|\n");
     printf("5|#| |#| |#| |#| |\n");
     printf("4| |#| |#| |#| |#|\n");
     printf("3|#| |#| |#| |#| |\n");
     printf("2|P|P|P|P|P|P|P|P|\n");
-    printf("1|L|H|C|F|K|C|H|L|\n");
+    printf("1|R|N|B|Q|K|B|N|R|\n");
     printf("  a b c d e f g h\n");
 }
